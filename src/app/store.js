ffbe63a3c1e9d22108ec1d829c393f46ae87bfe4
skumar <ch@ng3m3>
2023-03-08 11:13:50 -0600

    React Redux: Lesson 1 - basic app

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/conter/counterSlice";
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
